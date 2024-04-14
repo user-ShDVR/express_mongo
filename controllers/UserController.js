964d453619a5ff8d311b8d9c3d662a0e6e26c33a
Miroslav <bangwars3@gmail.com>
2024-04-15 01:34:01 +0400

    first commit

---
+++ controllers/UserController.js
@@ -0,0 +1,66 @@
+import UserModel from '../models/User.js';
+import bcrypt from 'bcrypt';
+
+export const getAllExceptCurrentUser = async (req, res) => {
+  try {
+    const users = await UserModel.find({ _id: { $ne: req.userId } }, { passwordHash: 0 });
+
+    if (!users || users.length === 0) {
+      return res.status(404).json({
+        message: 'Пользователи, кроме текущего, не найдены',
+      });
+    }
+
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: 'Произошла ошибка при получении пользователей, кроме текущего',
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const { email, ...restUpdates } = req.body;
+
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser && existingUser._id.toString() !== req.userId) {
+      return res.status(400).json({
+        message: 'Указанный email уже занят другим пользователем',
+      });
+    }
+
+    const updates = {};
+    if (email) updates.email = email;
+    if (restUpdates.name) updates.name = restUpdates.name;
+    if (restUpdates.birth_date) updates.birth_date = restUpdates.birth_date;
+    if (restUpdates.gender) updates.gender = restUpdates.gender;
+    if (restUpdates.avatarUrl) updates.avatarUrl = restUpdates.avatarUrl;
+    if (restUpdates.password) {
+      const salt = await bcrypt.genSalt(10);
+      updates.passwordHash = await bcrypt.hash(restUpdates.password, salt);
+    }
+
+    const user = await UserModel.findByIdAndUpdate(req.userId, updates, { new: true });
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'Пользователь не найден',
+      });
+    }
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: 'Произошла ошибка при обновлении профиля',
+    });
+  }
+};
+
+
---
+++ index.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import fs from 'fs';
+import multer from 'multer';
+import cors from 'cors';
+import 'dotenv/config'
+import mongoose from 'mongoose';
+
+import { registerValidation, loginValidation } from './validations.js';
+
+import { handleValidationErrors, checkAuth } from './utils/index.js';
+
+import { AuthController, UserController } from './controllers/index.js';
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err));
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/login', loginValidation, handleValidationErrors, AuthController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, AuthController.register);
+app.get('/auth/me', checkAuth, AuthController.getMe);
+app.get('/users', checkAuth, UserController.getAllExceptCurrentUser);
+app.put('/users/update', checkAuth, UserController.update);
+
+app.post('/upload', upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
+
+app.listen(process.env.PORT || 4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(`Server started on port ${process.env.PORT}`);
+});
